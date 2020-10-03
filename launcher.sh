6d1312e7e0f96c8e66dc18f6d7b27b34d13a9012
topshik <semenkin.anton@gmail.com>
2020-10-03 19:03:03 +0000

    add: CI/CD launcher

---
+++ launcher.sh
@@ -0,0 +1,19 @@
+cd nvidia-touch
+tmux kill-server
+git pull
+
+#conda activate web
+#sudo python touch/manage.py runserver 0.0.0.0:443 &
+#disown %%
+
+#cd nvidia-touch-frontend/
+#sudo npm start &
+#disown %%
+
+tmux new -d -s back
+tmux send-keys -t back C-z 'conda activate web' Enter
+tmux send-keys -t back C-z 'sudo /home/semenkin.anton/anaconda3/envs/web/bin/python touch/manage.py runserver 0.0.0.0:443' Enter
+
+cd nvidia-touch-frontend/
+tmux new -d -s main
+tmux send-keys -t main C-z 'sudo npm start' Enter
