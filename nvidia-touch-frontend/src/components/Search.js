c21ea895665b557f4c71f8c0c9eba6c13344eac8
The0nix <tktabolov@gmail.com>
2020-10-03 21:03:57 +0300

    fix database url

--- nvidia-touch-frontend/src/components/Search.js
+++ nvidia-touch-frontend/src/components/Search.js
@@ -3,7 +3,7 @@ import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-boot
 import axios from "axios";
 import logo from "../static/logo.jpeg"
 
-export const API_URL = "http://127.0.0.1:8000/api";
+export const API_URL = "http://nvidia-touch.com:443/api";
 
 export class Search extends Component {
     state = {
