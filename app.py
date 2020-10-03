214c07ab13abaafa218c9900b736907124718c25
Michael Vasilkovsky <waytobehigh@gmail.com>
2020-10-03 12:26:51 +0300

    feat: add placeholder app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8989, threaded=True, processes=1)
