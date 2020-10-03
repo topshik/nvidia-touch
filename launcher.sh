e31b3fd3289b2e12bd89ac17d88f4ea757bfc13f
Michael Vasilkovsky <waytobehigh@gmail.com>
2020-10-03 23:58:53 +0300

    fix: ci

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@ set -e
 sudo rm -rf nvidia-touch
 git clone git@github.com:topshik/nvidia-touch.git
 cd nvidia-touch
-kill $(pidof python) || :
-kill $(pidof screen) || :
+sudo kill $(pidof python) || :
+sudo kill $(pidof screen) || :
 #tmux kill-server
 #tmux start-server
 
@@ -26,6 +26,9 @@ kill $(pidof screen) || :
 #tmux send-keys -t main C-z 'sudo npm start' Enter
 
 #screen -d -m bash -c 'conda activate web && sudo /home/semenkin.anton/anaconda3/envs/web/bin/python touch/manage.py runserver 0.0.0.0:443'
+
+source ~/.bashrc
+
 conda activate web
 sudo /home/semenkin.anton/anaconda3/envs/web/bin/python touch/manage.py runserver 0.0.0.0:443 &
 disown %%
