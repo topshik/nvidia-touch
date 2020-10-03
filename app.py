96ef6de809d25e05c4ec0635039ce3502f0774b6
Michael Vasilkovsky <waytobehigh@gmail.com>
2020-10-03 12:41:41 +0300

    fix: hydra config path

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def hello_world():
     return "Hello, World!"
 
 
-@hydra.main(f'{os.getcwd()}/config/main.yaml')
+@hydra.main(config_name='config/main.yaml')
 def main(cfg: DictConfig):
     os.chdir(hydra.utils.get_original_cwd())
-    app.run(cfg.HOST, cfg.PORT, threaded=True, processes=1)
+    app.run(cfg.app.host, cfg.app.port, threaded=True, processes=1)
 
 
 if __name__ == '__main__':
