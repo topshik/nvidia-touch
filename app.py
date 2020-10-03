43206da27d2627ba81ae6ecc8efb74cadba0e5b0
topshik <semenkin.anton@gmail.com>
2020-10-03 14:51:32 +0300

    fix: host

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_project(project_id: int):
 @hydra.main(config_name='config/main.yaml')
 def main(cfg: DictConfig):
     os.chdir(hydra.utils.get_original_cwd())
-    app.run(cfg.app.host, cfg.app.port, threaded=True, processes=1)
+    app.run("0.0.0.0", cfg.app.port, threaded=True, processes=1)
 
 
 if __name__ == '__main__':
