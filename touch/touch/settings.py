6ffb8467160c5603ff6e90c64eaeca62a5baddfd
Your Name <you@example.com>
2020-10-03 16:45:06 +0300

    new shit

--- touch/touch/settings.py
+++ touch/touch/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'rest_framework',
+    'drf_yasg',
     'backend.apps.BackendConfig',
     
     'django.contrib.admin',
@@ -84,10 +85,13 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
+SWAGGER_SETTINGS = {
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
