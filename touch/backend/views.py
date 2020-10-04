4e38090954c2ba16eb62541426866494b5929623
Your Name <you@example.com>
2020-10-04 03:12:07 +0300

    create random coffee endpoint

--- touch/backend/views.py
+++ touch/backend/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, generics, permissions, mixins
+from rest_framework.response import Response
 
 from django.http import HttpResponse
 from .models import Employee, Project
 from .serializers import EmployeeSerializer, ProjectSerializer
 
+from django.core.serializers import serialize
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
@@ -16,5 +17,32 @@ class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = []
 
-def index(request):
-    return HttpResponse("Hello, world!")
+def CreateRandomCoffee(request, pk):
+    rand_employee = Employee.objects.filter(coffee_math=None).order_by('?').first()
+    myself = Employee.objects.get(pk=pk)
+
+    rand_employee.coffee_math = myself
+    rand_employee.save()
+
+    myself.coffee_math = rand_employee
+    myself.save()
+
+
+    serializer = EmployeeSerializer(rand_employee, context={'request': request})
+    return HttpResponse(str(serializer.data))
+
+def DeleteRandomCoffee(request, pk):
+    myself = Employee.objects.get(pk=pk)
+    other = myself.coffee_math
+
+
+    other.coffee_math = None
+    other.save()
+
+    myself.coffee_math = None
+    myself.save()
+
+
+    serializer = EmployeeSerializer(rand_employee, context={'request': request})
+    return HttpResponse(str(serializer.data))
+
